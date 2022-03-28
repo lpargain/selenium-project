8bb08fb1c02c8b78dc057e7e1b9c841cc89eb988
lpargain <102519372+lpargain@users.noreply.github.com>
2022-03-28 17:55:43 +0530

    Create BaseTest.java

---
+++ src/test/java/com/qa/baseTest/BaseTest.java
@@ -0,0 +1,66 @@
+package com.qa.baseTest;
+
+import com.pageactions.FilterSortingActions;
+import com.pageobjects.Locators;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class BaseTest {
+    public WebDriver driver;
+    private static final Properties prop = new Properties();
+    private static final Logger log = LogManager.getLogger(Logger.class.getName());
+    FilterSortingActions filterSortingActions;
+    Locators locators;
+
+    public BaseTest() {
+        filterSortingActions = new FilterSortingActions(driver);
+        locators = new Locators(driver);
+    }
+
+    @BeforeMethod
+    public void setUpTest() throws IOException {
+        InputStream inputStream = new FileInputStream("resource/config.properties");
+        prop.load(inputStream);
+        setUpFirefox();
+        log.info("Initializing browser");
+        driver.get(prop.getProperty("url"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        quit();
+    }
+
+    public void setUpChrome() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public void setUpFirefox() {
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+    }
+
+    public void quit() {
+        if (driver != null) {
+            driver.quit();
+            log.info("quitting browser");
+        }
+    }
+}
+
